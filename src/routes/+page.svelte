e830ce87a43a56a7916b59ef84edcb5ceacf9fb1
xubeiyan <yueyongdong@gmail.com>
2024-01-11 17:42:37 +0800

    chore(package.json): add static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,8 +22,8 @@
 	<title>Tmux 速查表 & 参考</title>
 </svelte:head>
 
-<div class="flex flex-col items-center w-full px-2 md:px-0 md:w-[30em] mx-auto">
-	<h1 class="text-2xl py-4 dark:text-white">Tmux 速查表 & 参考</h1>
+<div class="flex flex-col items-center w-full px-2 md:px-0 md:w-[40em] xl:w-[70em] mx-auto">
+	<h1 class="text-3xl py-8 dark:text-white">Tmux 速查表 & 参考</h1>
 	<label class="w-full group border border-slate-500 rounded-full p-2 flex">
 		<Search size={1.5} />
 		<input
@@ -36,7 +36,7 @@
 			</button>
 		{/if}
 	</label>
-	<div class="mt-2 w-full md:w-[45em] lg:w-[75em]">
+	<div class="mt-2 w-full md:w-[45em] xl:w-[75em]">
 		<Sessions sessions={filteredSesssions} />
 		<Windows windowData={filteredWindows} />
 	</div>
