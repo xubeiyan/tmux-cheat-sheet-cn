7ec844abcfbde4c3edc71c9f66b6568d3c5b8907
xubeiyan <yueyongdong@gmail.com>
2024-01-15 11:45:20 +0800

    fix(svelte.config.js): fix deploy problem of putting in sub folder of website

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			// 根据部署的站点的子目录修改
+			// 例如这个就是部署到chenhai.net/tools/tmux-cheat-sheet-cn 这个域名下
+			base: '/tools/tmux-cheat-sheet-cn'
+		}
 	},
 	preprocess: vitePreprocess()
 };
