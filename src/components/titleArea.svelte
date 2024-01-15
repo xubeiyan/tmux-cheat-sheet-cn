d4ffe0961a146959547e6eb25168578d26add1c1
xubeiyan <yueyongdong@gmail.com>
2024-01-15 11:22:46 +0800

    feature(github.svelte): add github link

--- src/components/titleArea.svelte
+++ src/components/titleArea.svelte
@@ -1,4 +1,12 @@
+<script>
+	import Github from '../assets/icon/github.svelte';
+</script>
+
 <h1 class="text-3xl pt-8 pb-4 dark:text-white">Tmux 速查表 & 参考</h1>
 <p class="text-sm mb-4 dark:text-slate-100">
 	翻译自<a class="underline px-1" href="https://tmuxcheatsheet.com/">tmuxcheatsheet.com</a>
+	勘误、贡献请戳
+	<a class="inline-block" target="_blank" href="https://github.com/xubeiyan/tmux-cheat-sheet-cn"
+		><Github /></a
+	>
 </p>
