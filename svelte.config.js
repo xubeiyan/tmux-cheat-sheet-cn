5386c670801219d5767992550cc4738621ca18e9
xubeiyan <yueyongdong@gmail.com>
2024-04-02 10:14:31 +0800

    chore(svelte.config.js): add different svelte.config.js configuration at baseURL when different environment

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
 		}),
 		paths: {
 			// 根据部署的站点的子目录修改
-			// 例如这个就是部署到chenhai.net/tools/tmux-cheat-sheet-cn 这个域名下
-			base: '/tools/tmux-cheat-sheet-cn'
+			// 例如生产环境就部署到 chenhai.net/tools/tmux-cheat-sheet-cn 这个域名下
+			base: process.env.NODE_ENV == 'production' ? '/tools/tmux-cheat-sheet-cn' : '',
 		}
 	},
 	preprocess: vitePreprocess()
